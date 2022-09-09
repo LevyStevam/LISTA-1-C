71527a51e28351d7279ed1ff9af0bc329ab44638
Levy Stevam Queiroz da Silva <stevamlevy@gmail.com>
2022-09-09 18:24:33 -0300

    Create lista1-022.c

---
+++ lista1-022.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+
+int main(){
+
+    printf("o número inteiro representado por 'A': %d \n", 'A');
+
+    printf("o número inteiro representado por 'B': %d \n", 'B');
+
+    printf("o número inteiro representado por 'C': %d \n", 'C');
+
+    printf("o número inteiro representado por 'a': %d \n", 'a');
+
+    printf("o número inteiro representado por 'b': %d \n", 'b');
+    
+    printf("o número inteiro representado por 'c': %d \n", 'c');
+    
+    printf("o número inteiro representado por '0': %d \n", '0');
+    
+    printf("o número inteiro representado por '1': %d \n", '1');
+    
+    printf("o número inteiro representado por '2': %d \n", '2');
+    
+    printf("o número inteiro representado por '$': %d \n", '$');
+   
+    printf("o número inteiro representado por '*': %d \n", '*');
+   
+    printf("o número inteiro representado por  '+': %d \n", '+');
+    
+    printf("o número inteiro representado por '/': %d \n", '/');
+    
+    printf("o número inteiro representado por 'SPACE': %d \n", ' ');
+    return 0;
+}
