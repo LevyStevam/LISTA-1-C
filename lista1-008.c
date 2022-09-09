038f9874f4a65f408a23cc635e54ec2294e7bafd
Levy Stevam Queiroz da Silva <stevamlevy@gmail.com>
2022-09-09 18:18:04 -0300

    Create lista1-008.c

---
+++ lista1-008.c
@@ -0,0 +1,7 @@
+#include <stdio.h>
+int main () {
+    int x;
+    puts("digite um número que deseje saber o quadrado: ");
+    scanf("%d", &x);
+    printf("o quadrado desse número é: %d", x*x);
+}
