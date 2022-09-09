f57e377e475c702e1a5ffb202e031ead875c0f40
Levy Stevam Queiroz da Silva <stevamlevy@gmail.com>
2022-09-09 18:06:35 -0300

    Create lista1-001.c

---
+++ lista1-001.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main()
+{
+    int x;
+    x = 7 + 3*6/2-1;
+    printf("x é: %d\n", x);
+    x = 2%2 + 2*2-2/2;
+    printf("x é: %d\n", x);
+    x = (3 * 9 * (3 + (9*3/ (3) ) ) );
+    printf("x é: %d\n", x);
+    
+
+    return 0;
+}
